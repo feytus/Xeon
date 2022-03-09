643fbd2cf2612e9c85d01ed557498033d39a1d86
Feytus <syheroyale@gmail.com>
2022-03-10 00:17:46 +0100

    update

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ import os
 packages = ['requests']
 [os.system(f'pip install {package}') for package in packages]
 os.system('cls')
+
 if not isfile('.env'):
     token = input("Enter the token of your bot : ")
     client_id = input("Enter your client_id | Enter 'None' if you don't want : ")
-    acces_token = input("Enter your acces_token | Enter 'None' if you don't want : ")
+    client_secret = input("Enter your client_secret | Enter 'None' if you don't want : ")
     with open('.env', 'a') as file:
-        file.write(f'token="{token}"\nclient_id="{client_id}"\nacces_token="{acces_token}"')
+        file.write(f'token="{token}"\nclient_id="{client_id}"\client_secret="{client_secret}"')
         file.close()
