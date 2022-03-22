612d3425433e01c080b220531c194cfc8adc8da3
Feytus <syheroyale@gmail.com>
2022-03-22 22:33:07 +0100

    adding colored logging

--- utils/logs.py
+++ utils/logs.py
@@ -2,13 +2,33 @@ import logging
 import os
 import datetime
 import sys
-import coloredlogs
+
+from colorama import Fore
+from logging import LogRecord
+
 
 full_date = datetime.datetime.now()
 date = full_date.strftime('%Y-%m-%d-%H-%M-%S')
 
 if "logs" not in os.listdir("."): 
     os.mkdir('logs')
+
+
+fmt = '%(asctime)s:%(name)s:{}%(levelname)s: %(message)s{}'
+
+FORMATS = {
+    logging.DEBUG: fmt.format(Fore.GREEN, Fore.RESET),
+    logging.INFO: fmt.format(Fore.CYAN, Fore.RESET),
+    logging.ERROR: fmt.format(Fore.RED, Fore.RESET),
+    logging.WARNING: fmt.format(Fore.YELLOW, Fore.RESET),
+    logging.CRITICAL: fmt.format(Fore.MAGENTA, Fore.RESET),
+}
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record: LogRecord) -> str:
+        log_fmt = FORMATS[record.levelno]
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
@@ -17,7 +37,7 @@ file_handler = logging.FileHandler(filename=f"logs/{date}.log", encoding='utf-8'
 file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 
 console_handler = logging.StreamHandler(sys.stdout)
-console_handler.formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s: %(message)s')
+console_handler.setFormatter(CustomFormatter(), style="%(")
 console_handler.setLevel(logging.INFO)
 
 logger.handlers = [file_handler, console_handler]
