8f19649916f5cf77ef647ae6f2f461c7eb235b05
Feytus <feytusinho@gmail.com>
2022-04-26 01:55:23 +0200

    Captcha verification

--- cogs/events.py
+++ cogs/events.py
@@ -1,8 +1,15 @@
 import datetime
 import discord
+import random
+import string
+import asyncio
+import pyimgur
+import os
+
+from captcha.image import ImageCaptcha
 
 from discord.ext import commands
-from discord import Bot, ApplicationContext, Embed, Member, Guild
+from discord import Bot, ApplicationContext, Embed, Member, Guild, PermissionOverwrite, Message
 
 from utils.warning import Warning
 from utils.color import Color
@@ -17,6 +24,7 @@ class Events(commands.Cog):
         self.bot: Bot = bot
         self.config = Config(bot)
         self.warning = Warning(bot)
+        self.imgur = pyimgur.Imgur(os.getenv("IMGUR_CLIENT_ID"))
 
         self.xeon = """
     ██╗  ██╗███████╗ █████╗ ███╗  ██╗
@@ -26,12 +34,14 @@ class Events(commands.Cog):
     ██╔╝╚██╗███████╗╚█████╔╝██║ ╚███║
     ╚═╝  ╚═╝╚══════╝ ╚════╝ ╚═╝  ╚══╝
     """
+
     @commands.Cog.listener()
     async def on_ready(self):
         print(self.xeon)
         logger.info(msg="latency : " + str(round(self.bot.latency * 1000)) + " ms")
 
         for guild in self.bot.guilds:
+            print(guild, self.config.is_config(guild))
             if not self.config.is_config(guild):
                 self.config.config_server(guild)
                 logger.info({"action": "configuration", "guild": {"id": guild.id, "name": guild.name}})
@@ -40,7 +50,7 @@ class Events(commands.Cog):
 
         logger.info(log)
 
-    @commands.Cog.listener()
+    #@commands.Cog.listener()
     async def on_application_command_error(self, ctx: ApplicationContext, error):
         await ctx.respond(
             embed=Embed(
@@ -55,7 +65,7 @@ class Events(commands.Cog):
         log = {"command": ctx.command, "author": ctx.author.id, "error": error}
         logger.warning(log)
 
-    @commands.Cog.listener()
+    #@commands.Cog.listener()
     async def on_error(self, ctx: ApplicationContext, error):
         if error is discord.errors.HTTPException:
             ctx.respond(embed=Embed(
@@ -66,6 +76,55 @@ class Events(commands.Cog):
                 ),
                 ephemeral=True
             )
+
+        log = {"command": ctx.command, "author": ctx.author.id, "error": error}
+        logger.warning(log)
+
+    async def captcha_check(self, user: Member):
+        if len(os.listdir(f"data/{guild.id}/captcha/")) >= 10:
+            for file in os.listdir(f"data/{guild.id}/captcha/"):
+                os.remove(f"data/{guild.id}/captcha/{file}")
+
+        guild = user.guild
+
+        overwrites = {
+            guild.default_role: PermissionOverwrite(read_messages=False),
+            guild.me: PermissionOverwrite(read_messages=True),
+            guild.get_member(user.id): PermissionOverwrite(read_messages=True),
+        }
+        channel = await guild.create_text_channel(name=user.name+"#"+user.discriminator, overwrites=overwrites)
+        
+        file_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".png"
+
+        letters = string.ascii_uppercase
+        result_str = ''.join(random.choice(letters) for i in range(5))
+
+        image = ImageCaptcha()
+
+        image.write(result_str, f"data/{guild.id}/captcha/{file_name}")
+
+        uploaded_image = self.imgur.upload_image(path=f"data/{guild.id}/captcha/{file_name}", title=file_name)
+        
+
+        embed = Embed(title="Captcha", description=f"{user.mention} Please send **{result_str}**", color=discord.Color.random())
+        embed.set_image(url=uploaded_image.link)
+        message_captcha = await channel.send(embed=embed)
+
+        try:
+            response: Message = await self.bot.wait_for("message", check=lambda response: response.author == user, timeout=30)
+        except asyncio.TimeoutError:
+            await user.kick()
+        
+        if response.content == result_str:
+            await user.add_roles(user.guild.get_role(871205238546255922))
+            await message_captcha.delete()
+            await response.delete()
+        else:
+            await user.kick()
+            await message_captcha.delete()
+            await response.delete()
+
+        await channel.delete()
 
     @commands.Cog.listener()
     async def on_member_join(self, user: Member):
@@ -82,9 +141,10 @@ class Events(commands.Cog):
 
             await user.guild.system_channel.send(embed=embed)
 
+        await self.captcha_check(user)
         log = {
             "action": "on_member_join", 
-            "user": {"id": user.id, "name": user.display_name+"#"+user.discriminator},
+            "user": {"id": user.id, "name": user.name+"-"+user.discriminator},
             "guild": user.guild.id
             }
 
